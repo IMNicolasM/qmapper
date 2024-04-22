1944b9b0975e5752f4b843c1147a2ef042a669f7
Noith <nmontealegre@imaginacolombia.com>
2024-04-22 18:27:33 -0500

    Added: logic to added new option in select

--- _crud/references.vue
+++ _crud/references.vue
@@ -3,7 +3,15 @@
 export default {
   data() {
     return {
-      crudId: this.$uid()
+      crudId: this.$uid(),
+      selectedColumnValue: null,
+      selectedUnifiedValue: null,
+      selectedUnifiedValue_Group: null,
+      selectedUnifiedValue_Category: null,
+      optionsColumnValue: [],
+      optionsUnifiedValue: [],
+      optionsUnifiedValue_Group: [],
+      optionsUnifiedValue_Category: []
     };
   },
   computed: {
@@ -19,7 +27,14 @@ export default {
         read: {
           columns: [
             { name: 'id', label: this.$tr('isite.cms.form.id'), field: 'id', align: 'left' },
-            { name: 'TableColumnName', label: 'Column name', field: 'TableColumnName', align: 'rigth', sortable: true },
+            {
+              name: 'TableColumnName',
+              label: 'Column name',
+              field: 'TableColumnName',
+              align: 'rigth',
+              sortable: true,
+              action: 'edit'
+            },
             { name: 'TablePK_EDW', label: 'Table PK (EDW)', field: 'TablePK_EDW', align: 'left' },
             {
               name: 'TableColumnValue',
@@ -122,12 +137,17 @@ export default {
         formLeft: {
           UNI_RefID: { value: '' },
           TableColumnName: {
-            value: '',
-            type: 'input',
+            value: null,
+            type: 'select',
             required: true,
             props: {
-              readonly: true,
+              readonly: this.crudInfo.typeForm === 'update',
               label: 'Column name'
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qmapper.references',
+              select: { label: 'TableColumnName', id: 'TableColumnName' },
+              requestParams: { filter: { _distinct: 'TableColumnName' } }
             }
           },
           TablePK_EDW: {
@@ -135,23 +155,42 @@ export default {
             type: 'input',
             required: true,
             props: {
-              readonly: true,
+              readonly: this.crudInfo.typeForm === 'update',
               label: 'Table PK (EDW)'
             }
           },
           TableColumnValue: {
-            value: '',
-            type: 'input',
+            value: this.selectedColumnValue,
+            type: 'select',
             required: true,
+            handler: {
+              label: 'Add New Column Value',
+              selectNewValue: (val) => {
+                this.optionsColumnValue.push({ label: val, value: val });
+                this.selectedColumnValue = val
+              }
+            },
             props: {
-              readonly: true,
-              label: 'Column value'
+              readonly: (this.crudInfo.typeForm === 'update' || !this.crudInfo.TableColumnName),
+              label: 'Column value',
+              options: this.optionsColumnValue,
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qmapper.references',
+              select: { label: 'TableColumnValue', id: 'TableColumnValue' },
+              requestParams: {
+                filter: {
+                  _distinct: 'TableColumnValue',
+                  TableColumnName: this.crudInfo.TableColumnName
+                }
+              },
+              filterByQuery: true,
+              filterLengthQuery: 0
             }
           },
           'Data owners': {
             value: '',
             type: 'input',
-            required: true,
             props: {
               readonly: true,
               label: 'Data owners'
@@ -159,33 +198,137 @@ export default {
           }
         },
         formRight: {
-          UnifiedValue: {
-            value: '',
-            type: 'input',
-            required: true,
+          MatchType: {
+            value: 'EXACT',
+            type: 'select',
+            require: true,
             props: {
-              readonly: true,
-              label: 'Unified Value'
+              label: 'Match type*',
+              options: [
+                { label: 'EXACT', value: 'EXACT' },
+                { label: 'PATTERN', value: 'PATTERN' }
+              ],
+              readonly: this.crudInfo.typeForm === 'update'
+            }
+          },
+          UnifiedValue: {
+            value: this.selectedUnifiedValue,
+            type: 'select',
+            required: true,
+            handler: {
+              label: 'Add New Unified Value',
+              selectNewValue: (val) => {
+                this.optionsUnifiedValue.push({ label: val, value: val });
+                this.selectedUnifiedValue = val
+              }
+            },
+            props: {
+              readonly: !this.crudInfo.TableColumnValue,
+              label: 'Unified Value',
+              options: this.optionsUnifiedValue
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qmapper.references',
+              select: { label: 'UnifiedValue', id: 'UnifiedValue' },
+              requestParams: {
+                filter: {
+                  _distinct: 'UnifiedValue',
+                  TableColumnName: this.crudInfo.TableColumnName,
+                  TableColumnValue: this.crudInfo.TableColumnValue
+                }
+              },
+              filterByQuery: true,
+              filterLengthQuery: 0
             }
           },
           UnifiedValue_Group: {
-            value: '',
-            type: 'input',
+            value: this.selectedUnifiedValue_Group,
+            type: 'select',
             required: true,
+            handler: {
+              label: 'Add New Unified Value Group',
+              selectNewValue: (val) => {
+                this.optionsUnifiedValue_Group.push({ label: val, value: val });
+                this.selectedUnifiedValue_Group = val
+              }
+            },
             props: {
-              readonly: true,
-              label: 'Unified Value Group'
+              readonly: !this.crudInfo.UnifiedValue,
+              label: 'Unified Value Group',
+              options: this.optionsUnifiedValue_Group
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qmapper.references',
+              select: { label: 'UnifiedValue_Group', id: 'UnifiedValue_Group' },
+              requestParams: {
+                filter: {
+                  _distinct: 'UnifiedValue_Group',
+                  TableColumnName: this.crudInfo.TableColumnName,
+                  TableColumnValue: this.crudInfo.TableColumnValue,
+                  UnifiedValue: this.crudInfo.UnifiedValue
+                }
+              },
+              filterByQuery: true,
+              filterLengthQuery: 0
             }
           },
           UnifiedValue_Category: {
-            value: '',
-            type: 'input',
+            value: this.selectedUnifiedValue_Category,
+            type: 'select',
             required: true,
+            handler: {
+              label: 'Add New Unified Value Category',
+              selectNewValue: (val) => {
+                this.optionsUnifiedValue_Category.push({ label: val, value: val });
+                this.selectedUnifiedValue_Category = val
+              }
+            },
             props: {
-              readonly: true,
-              label: 'Unified Value Category'
+              readonly: !this.crudInfo.UnifiedValue,
+              label: 'Unified Value Category',
+              options: this.optionsUnifiedValue_Category
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qmapper.references',
+              select: { label: 'UnifiedValue_Category', id: 'UnifiedValue_Category' },
+              requestParams: {
+                filter: {
+                  _distinct: 'UnifiedValue_Category',
+                  TableColumnName: this.crudInfo.TableColumnName,
+                  TableColumnValue: this.crudInfo.TableColumnValue,
+                  UnifiedValue: this.crudInfo.UnifiedValue
+                }
+              },
+              filterByQuery: true,
+              filterLengthQuery: 0
             }
           }
+        },
+        handleFormUpdates: (formData, changedFields, formType) => {
+          return new Promise(resolve => {
+            if (changedFields.length === 1) {
+              console.warn(formData, changedFields)
+              if(changedFields.includes('TableColumnName')) {
+                formData = {
+                  ...formData,
+                  TableColumnValue: null,
+                  UnifiedValue: null,
+                  UnifiedValue_Group: null,
+                  UnifiedValue_Category: null
+                };
+              }
+              else if(changedFields.includes('TableColumnValue')) {
+                formData = {
+                  ...formData,
+                  UnifiedValue: null,
+                  UnifiedValue_Group: null,
+                  UnifiedValue_Category: null
+                };
+              }
+
+            }
+            resolve(formData);
+          });
         }
       };
     },
